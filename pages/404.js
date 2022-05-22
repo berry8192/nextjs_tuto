268c40a463ade106ac33ea68497cfbb2de2a01eb
berry8192 <berryrt8192.gmail.com>
2022-05-22 20:06:54 +0900

    done_dynamic_route

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import styles from '../components/layout.module.css';
+import Link from 'next/link';
+
+// pages/404.js
+export default function Custom404() {
+    return (
+        <>
+            <h1>404 - Page Not Found</h1>
+            <div className={styles.backToHome}>
+                <Link href="/">
+                    <a>← Back to home</a>
+                </Link>
+            </div>
+        </>
+    );
+}
